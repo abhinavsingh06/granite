f4ec17878d6e28bacc02a4667d9d6a6c34b3d41b
Abhinav Singh <singh.abhinav6996@gmail.com>
2020-04-20 17:08:52 +0530

    sidekiq

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'bootstrap'
 gem 'react-rails'
 gem "js-routes"
 gem "pundit"
+gem "sidekiq"
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
---
+++ app/jobs/task_logger_job.rb
@@ -0,0 +1,18 @@
+class TaskLoggerJob < ApplicationJob
+  queue_as :default
+
+  before_enqueue :print_before_enqueue_message
+  after_enqueue :print_after_enqueue_message
+
+  def perform
+    puts "TaskLoggerJob is performed"
+  end
+
+  def print_before_enqueue_message
+    puts "Printing from inside before_enqueue callback"
+  end
+
+  def print_after_enqueue_message
+    puts "Printing from inside after_enqueue callback"
+  end
+end
